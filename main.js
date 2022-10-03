49221c280cd89602e616956b3c311184d657b8a3
edorojas93 <edo.rojas.10@gmail.com>
2022-10-03 10:50:01 -0500

    punto_20

--- main.js
+++ main.js
@@ -1,30 +1,20 @@
-addEventListener("DOMContentLoaded", (e)=>{
+function calcular(){
+    const sueldoBase = 1014980;
+    const comision = 0.10;
 
-    //Ciclo de WHILE//
-    let contador = 1, suma = 0, resta = 0, multiplicacion = 0, divsion = 0;
-    let cantidad = parseInt(prompt ("Ingrese la cantidad de procesos a realizar"));
-
-    while(cont <= cantidad)
-    {
+    let v1 = parseInt(document.getElementById('v1').value);
+    let v2 = parseInt(document.getElementById('v1').value);
+    let v3 = parseInt(document.getElementById('v1').value);
+    let subTotal = (v1 + v2 + v3) * comision;
+    let total = sueldoBase+ subTotal;
     
-    //Datos de entrada//
-    let num_1 = parseInt(prompt("Ingrese el primer numero:"));
-    let num_2 = parseInt(prompt("Ingrese el segundo numero: "));
+    document.getElementById('resultado').innerHTML="El sueldo total es de $" + total + " pesos <br>Comisión: $"+subTotal+" pesos";
+} 
 
-    //Proceso //
-    let suma = num_1 + num_2;
-    let resta = num_1 - num_2;
-    let multiplicacion = num_1 * num_2;
-    let division = num_1 / num_2;
-    cont ++;
-    }
-   
-    ///Salida//
-    document.write(`La suma de los dos numeros es:${suma}`)
-    document.write(`La resta de los dos numeros es:${resta}`)
-    document.write(`La multiplicacion de los dos numeros es:${multiplicacion}`)
-    document.write(`La division de los dos numeros es:${division}`)
-    })
+function limpiar(){
+    document.getElementById('miCalculadora').reset();
+    document.getElementById('resultado').innerHTML="";
+}
 
 
 
