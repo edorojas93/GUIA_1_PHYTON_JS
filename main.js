8bf3e72e490883e4b7c774015014f7bd392b9b9e
edorojas93 <edo.rojas.10@gmail.com>
2022-10-03 10:48:55 -0500

    punto_17

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-function calcular(){
-    const porc55 = 0.55;
-    const porc30 = 0.30;
-    const porc15 = 0.15;
-    let nota1 = parseFloat(document.getElementById('nota1').value);
-    let nota2 = parseFloat(document.getElementById('nota2').value);
-    let nota3 = parseFloat(document.getElementById('nota3').value);
-    let exmfin = parseFloat(document.getElementById('exmfin').value);
-    let trbfin = parseFloat(document.getElementById('trbfin').value);
-    let prom = ((nota1+nota2+nota3)/3) * porc55;
-    let promExamFin = exmfin * porc30;
-    let promTrbFin = trbfin * porc15;
-    let notaFinal = prom + promExamFin + promTrbFin; 
-    
-    document.getElementById('resultado').innerHTML=notaFinal;
-} 
+addEventListener("DOMContentLoaded", (e)=>{
 
-function limpiar(){
-    document.getElementById('miCalculadora').reset();
-    document.getElementById('resultado').innerHTML="";
+    //Ciclo de WHILE//
+    let contador = 1, suma = 0, resta = 0, multiplicacion = 0, divsion = 0;
+    let cantidad = parseInt(prompt ("Ingrese la cantidad de procesos a realizar"));
+
+    while(cont <= cantidad)
+    {
+    
+    //Datos de entrada//
+    let num_1 = parseInt(prompt("Ingrese el primer numero:"));
+    let num_2 = parseInt(prompt("Ingrese el segundo numero: "));
+
+    //Proceso //
+    let suma = num_1 + num_2;
+    let resta = num_1 - num_2;
+    let multiplicacion = num_1 * num_2;
+    let division = num_1 / num_2;
+    cont ++;
+    }
+   
+    ///Salida//
+    document.write(`La suma de los dos numeros es:${suma}`)
+    document.write(`La resta de los dos numeros es:${resta}`)
+    document.write(`La multiplicacion de los dos numeros es:${multiplicacion}`)
+    document.write(`La division de los dos numeros es:${division}`)
+    })
 
 
 
