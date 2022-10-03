c2fa6c39d51c3dde48bc98451da8c3124aa12fec
edorojas93 <edo.rojas.10@gmail.com>
2022-10-03 10:47:59 -0500

    punto_15

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-function calcular(){
-    const descuento = 0.10;
-    let costo = parseInt(document.getElementById('costo').value);
-    let subtotal = costo*descuento;
-    let total = costo - subtotal;
-    
-    document.getElementById('resultado').innerHTML="$"+total+" pesos";
-} 
+addEventListener("DOMContentLoaded", (e)=>{
 
-function limpiar(){
-    document.getElementById('miCalculadora').reset();
-    document.getElementById('resultado').innerHTML="";
-}
+    //Ciclo de WHILE//
+    let contador = 1, suma = 0, resta = 0, multiplicacion = 0, divsion = 0;
+    let cantidad = parseInt(prompt ("Ingrese la cantidad de procesos a realizar"));
+
+    while(cont <= cantidad)
+    {
+    
+    //Datos de entrada//
+    let num_1 = parseInt(prompt("Ingrese el primer numero:"));
+    let num_2 = parseInt(prompt("Ingrese el segundo numero: "));
+
+    //Proceso //
+    let suma = num_1 + num_2;
+    let resta = num_1 - num_2;
+    let multiplicacion = num_1 * num_2;
+    let division = num_1 / num_2;
+    cont ++;
+    }
+   
+    ///Salida//
+    document.write(`La suma de los dos numeros es:${suma}`)
+    document.write(`La resta de los dos numeros es:${resta}`)
+    document.write(`La multiplicacion de los dos numeros es:${multiplicacion}`)
+    document.write(`La division de los dos numeros es:${division}`)
+    })
+
 
 
             
